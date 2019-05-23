676f605240944aa14e4eb1e1ef4612fa579202ba
Bryan Teng <bteng1226@gmail.com>
2019-05-23 11:53:48 -0500

    done

--- password.js
+++ password.js
@@ -1,1 +1,16 @@
-// code solution here
+let args = process.argv.filter(x=> +x == x)
+
+function generatePasswords(length){
+    let alphabet ='abcdefghijklmnopqrstuvwxyz'
+    let temp = ""
+    for(let i=0; i<length;i++){
+        temp+= alphabet[~~Math.random()*26]
+    }
+    return temp
+}
+
+for(let i=0; i<args[1]; i++){
+    console.log(generatePasswords(args[0]))
+}
+
+process.exit(0)
